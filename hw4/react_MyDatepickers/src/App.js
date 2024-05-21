7af55776548475492112eadf42f2b3e64307bc5c
AlisaShi <59513354+AlisaShi@users.noreply.github.com>
2024-05-21 23:55:21 +0800

    0521

---
+++ hw4/react_MyDatepickers/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import DatePickers from './birthday';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DatePickers />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
